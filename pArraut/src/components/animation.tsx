cf2181f874ff6ff62b3215950959279c6cadc42e
PaolaArraut <paoarraut05@gmail.com>
2026-07-14 20:23:28 -0400

    ..similar changes

--- pArraut/src/components/animation.tsx
+++ pArraut/src/components/animation.tsx
@@ -1,9 +1,15 @@
+import React from "react";
+import { useState, useEffect, useMemo } from "react";
+import 'pArraut/public/assets/animation.bin'
+
+const ANIMATION_PATH = 'pArraut/public/assets/animation.bin';
+
 
 
 function Animation() {
   //For the animation I need to fetch the bin
   // split into frames
-  // Then advance on a timer
+  // Then advance on a timer within 30 FPS
 
   return null;
 }
