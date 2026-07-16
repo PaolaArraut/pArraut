28ba97391d0fc7f29413c8b832e4e58becc7c3bb
PaolaArraut <paoarraut05@gmail.com>
2026-07-15 20:57:48 -0400

    Small incrementations

--- pArraut/src/components/animation.tsx
+++ pArraut/src/components/animation.tsx
@@ -7,6 +7,13 @@ const ANIMATION_PATH = 'pArraut/public/assets/animation.bin';
 
 
 function Animation() {
+  const [frames, setFrames] = useState<string[]>([])
+  const { frameIndex, setFrameIndex } = useState(0);
+
+  const useEffect
+
+
+
   //For the animation I need to fetch the bin
   // split into frames
   // Then advance on a timer within 30 FPS
